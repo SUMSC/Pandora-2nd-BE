d9bf5dc1187120914fed663974025b29b607e167
LionTao <taojiachun980831@163.com>
2019-05-13 11:45:27 +0800

    add user

--- main/__init__.py
+++ main/__init__.py
@@ -49,8 +49,8 @@ def create_app(test_config=None):
             test_grade = db.execute('''
                 SELECT * FROM test WHERE user_id IN (
                     SELECT id FROM user WHERE username = ?
-                ) ORDER BY test_time''', (username, )
-            ).fetchall()
+                ) ORDER BY test_time''', (username,)
+                                    ).fetchall()
             if not test_grade:
                 return jsonify({"error": "no such user"})
             return jsonify(list(map(
@@ -58,13 +58,54 @@ def create_app(test_config=None):
                 test_grade)))
         if request.method == 'POST':
             data = request.json
-            uid = db.execute("""SELECT id FROM user WHERE username = ?""", (data['username'], )).fetchone()['id']
+            uid = db.execute("""SELECT id FROM user WHERE username = ?""", (data['username'],)).fetchone()['id']
             db.execute("""INSERT INTO test(user_id, test_status, error_log) VALUES (?, ?, ? )""",
                        (uid, data['test_status'], data['error_log']))
             try:
                 db.commit()
                 return jsonify({'message': 'success'})
             except Exception as e:
+                print(e)
+                return "error"
+
+    @app.route('/user', methods=['GET', 'POST'])
+    def user():
+        db = get_db()
+        if request.method == 'GET':
+            """
+            get user info
+            """
+            id_tag = request.args.get('id_tag')
+            userinfo = db.execute('''
+                SELECT USERNAME,ID_TAG,REPO FROM user WHERE id_tag=?
+            ''', (id_tag,)
+                                  ).fetchall()
+            if not userinfo:
+                return jsonify({"error": "no such user"})
+            return jsonify(list(map(
+                lambda item: dict(zip(item.keys(), tuple(item))),
+                userinfo)))
+        elif request.method == 'POST':
+            """
+            JSON Format:
+            {
+                "id_tag": "<string>",
+                "username": "<string>",
+                "repo": "<string> (Must be github repo url in https mode or just 'None')"
+            }
+            """
+            data = request.json
+            uid = db.execute("""SELECT id FROM user WHERE id_tag = ?""", (data['id_tag'],)).fetchone()['id']
+            if uid:
+                return jsonify({"error": "user exists"})
+            else:
+                db.execute("""INSERT INTO user(username, id_tag, repo) VALUES (?, ?, ? )""",
+                           (data['username'], data['id_tag'], data['repo']))
+            try:
+                db.commit()
+                return jsonify({'message': 'success'})
+            except Exception as e:
+                db.rollback()
                 print(e)
                 return "error"
 
