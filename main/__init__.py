71169dbeff3ea6922712d7827bcc6b3df473ff90
LionTao <taojiachun980831@163.com>
2019-05-20 16:18:39 +0800

    add grade ratio

--- main/__init__.py
+++ main/__init__.py
@@ -41,6 +41,17 @@ def create_app(test_config=None):
     def index():
         return 'here is nothing to show.'
 
+    @app.route('/inspect/gradenum', methods=['GET'])
+    def gradenum():
+        db = get_db()
+        try:
+            num = db.execute('''
+                                select count(id) as value from test
+                                ''').fetchone()
+            return jsonify([{"name": "判题次数", "value": num[0], "prefix": "共", "suffix": "次"}])
+        except Exception as e:
+            return jsonify({"error": str(e)})
+
     @app.route('/inspect/usernum', methods=['GET'])
     def usernum():
         db = get_db()
