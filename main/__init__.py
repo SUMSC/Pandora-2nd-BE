4674c96cc9715f7516fff99ddf4db816faeae69a
LionTao <taojiachun980831@163.com>
2019-05-20 10:25:41 +0800

    add repo to grade

--- main/__init__.py
+++ main/__init__.py
@@ -113,8 +113,9 @@ def create_app(test_config=None):
                 return jsonify({'error': 'no such user'})
 
             try:
-                db.execute("""INSERT INTO test(user_id, test_status, error_log,repo) VALUES (?, ?, ? ,?)""",
-                           (uid, data.get('test_status'), data.get('error_log'), data.get('repo')))
+                db.execute("""INSERT INTO test(user_id, test_status, test_grade,error_log,repo) VALUES (?, ?, ? ,?)""",
+                           (uid, data.get('test_status'), data.get('test_grade'), data.get('error_log'),
+                            data.get('repo')))
                 db.commit()
                 return jsonify({'message': 'success'})
             except Exception as e:
--- main/schema.sql
+++ main/schema.sql
@@ -1,19 +1,22 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS test;
 
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username VARCHAR(10) UNIQUE NOT NULL,
-  id_tag VARCHAR(20) UNIQUE NOT NULL,
-  repo TEXT UNIQUE
+CREATE TABLE user
+(
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(10) UNIQUE NOT NULL,
+    id_tag   VARCHAR(20) UNIQUE NOT NULL,
+    repo     TEXT UNIQUE
 );
 
-CREATE TABLE test (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER NOT NULL,
-  test_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  test_status INTEGER NOT NULL,
-  error_log TEXT NOT NULL,
-  repo TEXT UNIQUE,
-  FOREIGN KEY (user_id) REFERENCES user (id)
+CREATE TABLE test
+(
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id     INTEGER   NOT NULL,
+    test_time   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    test_status TEXT      NOT NULL,
+    test_grade  TEXT      NOT NULL,
+    error_log   TEXT      NOT NULL,
+    repo        TEXT UNIQUE,
+    FOREIGN KEY (user_id) REFERENCES user (id)
 );
